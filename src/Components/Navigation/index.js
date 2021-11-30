9d63f72d927af4abe9ec2a9a7509be16d195dbbc
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-11-30 12:12:50 +0530

    Undo Changes

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -10,7 +10,6 @@ function Navigation() {
                     <Link to="/order" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Order</Link>
                     <Link to="/kitchen" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Kitchen</Link>
                     <Link to="/dashboard/cashier" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink">Dashboard</Link>
-                    <a href="/customer-safety" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink">Dashboard New</a>
                 </div>
             </div>
         </div>
