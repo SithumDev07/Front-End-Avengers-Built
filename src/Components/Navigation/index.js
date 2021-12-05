24c25f325fd82b0e0b8213e365e50b791e2f7e36
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-12-05 21:29:50 +0530

    SVG Errors

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -7,7 +7,7 @@ function Navigation() {
             <div className="container mx-auto">
                 <div className="flex flex-col items-start justify-center">
                     <Link to="/customer-safety" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Customer Safety</Link>
-                    <Link to="/order" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Order</Link>
+                    {/* <Link to="/order" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Order</Link> */}
                     <Link to="/kitchen" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink mb-6">Kitchen</Link>
                     <Link to="/dashboard/cashier" className="px-6 py-4 rounded-lg hover:text-gray-300 bg-pink">Dashboard</Link>
                 </div>
--- src/Components/RootBack/index.js
+++ src/Components/RootBack/index.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function RootBack() {
     return (
         <Link to="/" className="fixed hover:text-gray-100 z-50 left-10 top-6 p-2 rounded-full bg-pink text-gray-50">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
         </Link>
     )
