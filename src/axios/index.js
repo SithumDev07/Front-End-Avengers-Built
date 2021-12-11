cea0a6a8b621f115584538f11f831e7ee3cae2c6
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-12-11 14:33:53 +0530

    backend URL changed

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://avengerscafebackend-env.eba-4k2yn9bw.us-east-2.elasticbeanstalk.com"
+    baseURL: "https://www.ceylonpickup.com"
 })
 
 export default instance;
